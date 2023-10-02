d83a725b5506e11474b05f94d50078a06aba0f39
Brandon Fraune <brandon.fraune@gmail.com>
2023-10-02 15:51:40 -0500

    project init

---
+++ CuckooSwiftTest/CuckooSwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuckooSwiftTestApp.swift
+//  CuckooSwiftTest
+//
+//  Created by Brandon Fraune on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CuckooSwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
